266b8509040b113ad785dbd67028c461fd2687f5
Andrew Rea <email@andrewrea.co.uk>
2017-03-10 11:22:38 +0000

    Added nginx to the mix

--- deploy/main.tf
+++ deploy/main.tf
@@ -67,8 +67,8 @@ resource "aws_security_group" "default" {
 
   # HTTP access from the VPC
   ingress {
-    from_port   = 8008
-    to_port     = 8008
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
